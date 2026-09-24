341cfd362fc00a08e96e3b83221f2f4ac1ff32ec
karisaa123o-hub <karisaa123o@gmail.com>
2026-09-24 15:07:29 +0700

    update

--- JelajahPo_Backend/server.js
+++ JelajahPo_Backend/server.js
@@ -32,6 +32,14 @@ app.get('/wisata', (req, res) => {
     });
 });
 
+app.get('/kategori', (req, res) => {
+    const sql = 'SELECT * FROM kategori';
+    db.query(sql, (err, results) => {
+        if (err) return res.status(500).json({ error: err });
+        res.json(results);
+    });
+});
+
 app.listen(PORT, ()=> {
     console.log(`Server JelajahPo jalan di http://localhost:${PORT}`)
 })
