5758e157300f540538670ffbe844e9ebf0bc1c0a
karisaa123o-hub <karisaa123o@gmail.com>
2026-09-25 14:16:44 +0700

    update

---
+++ jelajahpo-frontend/src/pages/Tentang.jsx
@@ -0,0 +1,17 @@
+export default function Tentang() {
+    return (
+        <div style={{ padding: "20px" }}>
+            <h2>Tentang</h2>
+
+            <p><strong>Nama :</strong>Karissa</p>
+            <p><strong>Kelas :</strong>XII RPL 1</p>
+
+            <h3>Destinasi Wisata Favorit</h3>
+            <p>
+                What if destinasi wisata favorit yang paling ingin saya kunjungi adalah berbagai tempat indah di dunia, seperti Lauterbrunnen, Amsterdam Museum Van Gogh, dan Ayutthaya.
+                Saya ingin menikmati pemandangan alam yang menakjubkan, mengunjungi tempat-tempat bersejarah, serta mengenal budaya yang berbeda.
+                Tidak lupa, saya juga sangat ingin pergi ke Makkah untuk beribadah dan merasakan suasana yang penuh kedamaian. Aamiin in saja ya Allah
+            </p>
+        </div>
+    );
+}
