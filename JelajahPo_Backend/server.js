69cfb2723b005ec47f8ae40dcf351d5a687f1342
karisaa123o-hub <karisaa123o@gmail.com>
2026-09-25 20:23:26 +0700

    update

--- JelajahPo_Backend/server.js
+++ JelajahPo_Backend/server.js
@@ -51,6 +51,10 @@ app.post('/wisata', (req, res) => {
         return res.status(400).json({ message: 'Nama Wisata dan harga_tiket wajib diisi' });
     }
 
+    if (!deskripsi || !deskripsi.trim()) {
+        return res.status(400).json({ message: 'Deskripsi wajib diisi!' });
+    }
+
     const sql = 'INSERT INTO wisata (nama_wisata, deskripsi, harga_tiket, id_kategori, tgl_input) VALUES (?, ?, ?, ?, NOW())';
     db.query(sql, [nama_wisata, deskripsi, harga_tiket, id_kategori], (err, result) => {
         if (err) return res.status(500).json({ error: err.sqlMessage });
